ef078547f2045df03cdaf454df04194427718fcb
tokwalover666 <91291v@gmail.com>
2024-03-05 17:28:01 +0800

    on&off flashlight anim done

---
+++ Assets/Scripts/HandsAnimation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandsAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private bool isFlashlightOn = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isFlashlightOn = !isFlashlightOn;
+
+            animator.SetBool("IsFlashlightOn", isFlashlightOn);
+        }
+    }
+}
