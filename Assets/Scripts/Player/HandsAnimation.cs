f8aaeb58f782bc2084840ce47e7c657898da9684
cicerolb <115563856+cicerolb@users.noreply.github.com>
2024-03-08 00:20:10 +0800

    Added official prologue scene
    and code changes

--- Assets/Scripts/Player/HandsAnimation.cs
+++ Assets/Scripts/Player/HandsAnimation.cs
@@ -8,26 +8,56 @@ public class HandsAnimation : MonoBehaviour
     [SerializeField] public bool isFlashlightOn = false;
     [SerializeField] public bool flashlightAnimation = false;
     [SerializeField] private DialPuzzleManager1 dialPuzzleManager;
+    [SerializeField] private ProloguePuzzleManager prologuePuzzleManager;
     [SerializeField] private bool puzzleStart;
+    [SerializeField] private GameObject currentPuzzle;
 
 
     void Start()
     {
-        dialPuzzleManager = GameObject.FindGameObjectWithTag("Dial Puzzle").GetComponent<DialPuzzleManager1>();
         animator = GetComponent<Animator>();
+
+        currentPuzzle = GameObject.FindGameObjectWithTag("Puzzle");
+
+        if (currentPuzzle.name == "DialPuzzle")
+        {
+            dialPuzzleManager = GameObject.FindGameObjectWithTag("Puzzle").GetComponent<DialPuzzleManager1>();
+
+        }
+        else if (currentPuzzle.name == "ProloguePuzzle")
+        {
+            prologuePuzzleManager = GameObject.FindGameObjectWithTag("Puzzle").GetComponent<ProloguePuzzleManager>();
+        }
     }
 
     void Update()
     {
         animator.SetBool("IsFlashlightOn", flashlightAnimation);
-        if (dialPuzzleManager.dialPuzzleStart)
+        if (dialPuzzleManager != null)
         {
-            puzzleStart = true;
+            if (dialPuzzleManager.dialPuzzleStart)
+            {
+                puzzleStart = true;
+            }
+            else
+            {
+                puzzleStart = false;
+            }
         }
-        else
+        else if (prologuePuzzleManager != null)
         {
-            puzzleStart = false;
+            if (prologuePuzzleManager.prologuePuzzleStart)
+            {
+                puzzleStart = true;
+            }
+            else
+            {
+                puzzleStart = false;
+            }
         }
+
+
+
 
 
         if (!puzzleStart)
