00a937368af32dbe2f5dc7b90a154012ebebaea4
cicerolb <115563856+cicerolb@users.noreply.github.com>
2024-03-16 16:53:25 +0800

    sada

--- Assets/Scripts/Game/AudioManager.cs
+++ Assets/Scripts/Game/AudioManager.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    // Audio Sources
     [SerializeField] AudioSource footSteps;
     [SerializeField] AudioSource enemyFootStepsSound;
-    public bool enemyFootSteps = false;
     [SerializeField] AudioSource jumpScareSound;
+    [SerializeField] AudioSource chaseSound;
+
+    // Bools
+    public bool enemyFootSteps = false;
     public bool jumpScare = false;
+    public bool chase = false;
+    public bool chaseStop = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +25,41 @@ public class AudioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        EnemyFootStepsAudio();
+        JumpScareAudio();
+        ChaseAudio();
+    }
+
+    void EnemyFootStepsAudio()
+    {
         if (enemyFootSteps)
         {
             enemyFootStepsSound.Play();
             enemyFootSteps = false;
         }
+    }
 
+    void JumpScareAudio()
+    {
         if (jumpScare)
         {
             jumpScareSound.Play();
             jumpScare = false;
         }
     }
+
+    void ChaseAudio()
+    {
+        if (chase)
+        {
+            chaseSound.Play();
+            chase = false;
+        }
+
+        if (chaseStop)
+        {
+            chaseSound.Stop();
+        }
+    }
+
 }
