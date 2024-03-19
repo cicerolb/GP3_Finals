e7e7b3dbb2d0495a49e02aafcbf78af655e66231
cicerolb <115563856+cicerolb@users.noreply.github.com>
2024-03-19 23:42:04 +0800

    asdasdasd

--- Assets/FlashlightIntensity.cs
+++ Assets/FlashlightIntensity.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class FlashlightIntensity : MonoBehaviour
 {
+    [SerializeField] Light light;
+    [SerializeField] float[] lightPower;
+    [SerializeField] int i;
     // Start is called before the first frame update
     void Start()
     {
+     
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            i = (i + 1) % lightPower.Length;
+        }
+
+
+
+
+
+
+        light.intensity = lightPower[i];
     }
 }
